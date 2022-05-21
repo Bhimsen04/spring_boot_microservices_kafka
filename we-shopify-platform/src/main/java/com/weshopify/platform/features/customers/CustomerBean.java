18c72905b79ebc7d8a4f8aa2c8c62e2155f03804
Bhimsen04 <bhimsen1304@gmail.com>
2022-05-21 18:27:16 +0530

    Custom Validation, javax.validation, maked own annotation , @interface PasswordValidator, ConstraintValidator

--- we-shopify-platform/src/main/java/com/weshopify/platform/features/customers/CustomerBean.java
+++ we-shopify-platform/src/main/java/com/weshopify/platform/features/customers/CustomerBean.java
@@ -1,9 +1,12 @@
 package com.weshopify.platform.features.customers;
 
 import java.io.Serializable;
-import java.util.Objects;
+import javax.validation.constraints.Pattern;
 
-import org.springframework.web.bind.annotation.RequestParam;
+import com.weshopify.platform.features.customers.commons.PasswordValidator;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 
 public class CustomerBean implements Serializable {
 
@@ -12,13 +15,28 @@ public class CustomerBean implements Serializable {
 	 */
 	private static final long serialVersionUID = -3392255258797133035L;
 	private int customerId;
+	private boolean selfReg;
+
+	@NotEmpty(message = "first name shouldn't be empty")
 	private String firstName;
+
+	@NotEmpty(message = "last name shouldn't be empty")
 	private String lastName;
+
+	@NotEmpty(message = "user name shouldn't be empty")
 	private String userName;
+
+	@NotEmpty(message = "email shouldn't be empty")
+	@Email(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
 	private String email;
+
+	@Pattern(regexp = "^([+]\\d{2})?\\d{10}$", message = "Mobile Number should be of minimum 10 digits")
 	private String mobileNumber;
+
+	@NotEmpty(message = "password shouldn't be empty")
+	@PasswordValidator(message = "Password should be 8 characters Length with the "
+			+ "One Letter must be Capital and One Number Should Present")
 	private String password;
-	private boolean selfReg = false;
 
 	public boolean isSelfReg() {
 		return selfReg;
